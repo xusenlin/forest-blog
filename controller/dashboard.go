72793845616e4b1e2e850f1ef8af26acd5398ac0
五木老祖 <wumulaozu@gmail.com>
2019-09-17 14:20:42 +0800

    添加手动更新文章功能

--- controller/dashboard.go
+++ controller/dashboard.go
@@ -9,15 +9,25 @@ import (
 )
 
 func Dashboard(w http.ResponseWriter, r *http.Request)  {
+
+	var dashboardMsg []string
+
 	err := r.ParseForm()
 	if err != nil {
 		helper.WriteErrorHtml(w, err.Error())
 		return
 	}
 
-	index, err := strconv.Atoi(r.Form.Get("color_index"))
+	index, err := strconv.Atoi(r.Form.Get("theme"))
 	if err == nil && index < len(config.Cfg.ThemeOption) {
 		service.SetThemeColor(index)
+		dashboardMsg = append(dashboardMsg,"颜色切换成功!")
+	}
+
+	action := r.Form.Get("action")
+	if "updateArticle" == action {
+		helper.UpdateArticle()
+		dashboardMsg = append(dashboardMsg,"文章更新成功!")
 	}
 
 	template, err := helper.HtmlTemplate("dashboard")
@@ -29,7 +39,9 @@ func Dashboard(w http.ResponseWriter, r *http.Request)  {
 
 	err = template.Execute(w, map[string]interface{}{
 		"Title":  "控制台",
-		"Data":   "",
+		"Data":   map[string]interface{}{
+			"msg":dashboardMsg,
+		},
 		"Config": config.Cfg,
 	})
 	if err != nil {
