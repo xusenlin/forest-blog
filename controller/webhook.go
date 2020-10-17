c56f669f0220aa8f59f80dc4e80eed7307d4ef23
五木老祖 <wumulaozu@gmail.com>
2020-10-17 13:22:11 +0800

    去掉秘钥验证

--- controller/webhook.go
+++ controller/webhook.go
@@ -1,56 +1,50 @@
 package controller
 
 import (
-	"ForestBlog/config"
 	"ForestBlog/models"
-	"crypto/hmac"
-	"crypto/sha1"
-	"encoding/hex"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 func GithubHook(w http.ResponseWriter, r *http.Request) {
 
-	err := r.ParseForm()
-	if err != nil {
-		SedResponse(w, err.Error())
-		return
-	}
-
-	if "" == config.Cfg.WebHookSecret || "push" != r.Header.Get("x-github-event") {
-		SedResponse(w, "No Configuration WebHookSecret Or Not Pushing Events")
-		log.Println("No Configuration WebHookSecret Or Not Pushing Events")
-		return
-	}
-
-	sign := r.Header.Get("X-Hub-Signature")
-
-	bodyContent, err := ioutil.ReadAll(r.Body)
-
-	if err != nil {
-		SedResponse(w, err.Error())
-		log.Println("WebHook err:" + err.Error())
-		return
-	}
-
-	if err = r.Body.Close(); err != nil {
-		SedResponse(w, err.Error())
-		log.Println("WebHook err:" + err.Error())
-		return
-	}
-
-	mac := hmac.New(sha1.New, []byte(config.Cfg.WebHookSecret))
-	mac.Write(bodyContent)
-	expectedHash := "sha1=" + hex.EncodeToString(mac.Sum(nil))
-
-	if sign != expectedHash {
-		SedResponse(w, "WebHook err:Signature does not match")
-		log.Println("WebHook err:Signature does not match")
-		return
-	}
+	//err := r.ParseForm()
+	//if err != nil {
+	//	SedResponse(w, err.Error())
+	//	return
+	//}
+	//
+	//if "" == config.Cfg.WebHookSecret || "push" != r.Header.Get("x-github-event") {
+	//	SedResponse(w, "No Configuration WebHookSecret Or Not Pushing Events")
+	//	log.Println("No Configuration WebHookSecret Or Not Pushing Events")
+	//	return
+	//}
+	//
+	//sign := r.Header.Get("X-Hub-Signature")
+	//
+	//bodyContent, err := ioutil.ReadAll(r.Body)
+	//
+	//if err != nil {
+	//	SedResponse(w, err.Error())
+	//	log.Println("WebHook err:" + err.Error())
+	//	return
+	//}
+	//
+	//if err = r.Body.Close(); err != nil {
+	//	SedResponse(w, err.Error())
+	//	log.Println("WebHook err:" + err.Error())
+	//	return
+	//}
+	//
+	//mac := hmac.New(sha1.New, []byte(config.Cfg.WebHookSecret))
+	//mac.Write(bodyContent)
+	//expectedHash := "sha1=" + hex.EncodeToString(mac.Sum(nil))
+	//
+	//if sign != expectedHash {
+	//	SedResponse(w, "WebHook err:Signature does not match")
+	//	log.Println("WebHook err:Signature does not match")
+	//	return
+	//}
 
 	SedResponse(w, "ok")
 
